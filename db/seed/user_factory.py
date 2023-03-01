6df8f6137f05cfb02967d84fdd29db5958b8b315
Jonathan Osmond <jonathancosmond@gmail.com>
2023-02-28 23:45:04 -0500

    continue seed.py - actually commits now

--- db/seed/user_factory.py
+++ db/seed/user_factory.py
@@ -1,14 +1,15 @@
 import datetime
 import factory
 import random
-from db.setup import get_scoped_session
+from db.setup import get_scoped_session 
 from db.models import User
 
 
 class UserFactory(factory.alchemy.SQLAlchemyModelFactory):
     class Meta:
         model = User
-        sqlalchemy_session = get_scoped_session()
+        sqlalchemy_session = get_scoped_session() 
+        sqlalchemy_session_persistence = "commit"
 
     # username = factory.Sequence(lambda n: "john%s" % n)
     # email = factory.LazyAttribute(lambda o: "%s@example.org" % o.username)
