18aa26f8984cc67185a868106656b3156e01ce23
Jonathan Osmond <jonathancosmond@gmail.com>
2023-03-01 09:33:13 -0500

    improve seeding script and randomzie userfactory fields

--- db/seed/user_factory.py
+++ db/seed/user_factory.py
@@ -1,11 +1,12 @@
 import datetime
-import factory
+from factory import Faker
+from factory.alchemy import SQLAlchemyModelFactory
 import random
 from db.setup import get_scoped_session 
 from db.models import User
 
 
-class UserFactory(factory.alchemy.SQLAlchemyModelFactory):
+class UserFactory(SQLAlchemyModelFactory):
     class Meta:
         model = User
         sqlalchemy_session = get_scoped_session() 
@@ -15,7 +16,12 @@ class UserFactory(factory.alchemy.SQLAlchemyModelFactory):
     # email = factory.LazyAttribute(lambda o: "%s@example.org" % o.username)
     # date_joined = factory.LazyFunction(datetime.datetime.now)
 
-    email = "test"
-    first_name = "test"
-    last_name = "test"
-    hashed_password = "test"
+    email = Faker('ascii_email')
+    email = Faker('ascii_email')
+    email_verified = Faker('boolean', 
+                           chance_of_getting_true=75)
+    first_name = Faker('first_name')
+    last_name = Faker('last_name')
+    disabled = Faker('boolean', 
+                     chance_of_getting_true=5)
+    hashed_password = "$2b$12$lyS1PwcbotaTYuBaNyl/Wes41F756lF.qndgFd9BpaL/7FB6QMFbq"
--- seed.py
+++ seed.py
@@ -1,5 +1,19 @@
 from db.seed.user_factory import UserFactory
+from sqlalchemy import delete
+from sqlalchemy.orm import Session
+from db.setup import get_db
+from db.models import User
 
 if __name__ == "__main__":
+    #reset db
+    print("Resetting DB...")
+    session: Session = next(get_db())
+    stmt = delete(User)
+    session.execute(stmt)
+    session.commit()
+    print("Resetting DB: DONE")
+
+    #seed
     print("Seeding DB...")
-    UserFactory.create()
+    UserFactory.create_batch(50)
+    print("Seeding DB: DONE")
