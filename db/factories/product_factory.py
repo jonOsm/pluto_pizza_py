69827942339c591ee78457f8836ff4b832179ccd
Jonathan Osmond <jonathancosmond@gmail.com>
2023-03-02 13:47:31 -0500

    improve name field in productfactory

--- db/factories/product_factory.py
+++ db/factories/product_factory.py
@@ -1,4 +1,4 @@
-from factory import Faker
+from factory import Faker, LazyAttribute
 from factory.alchemy import SQLAlchemyModelFactory
 from db.setup import get_scoped_session
 from db.models import User, Product
@@ -7,13 +7,17 @@ from db.models import User, Product
 class ProductFactory(SQLAlchemyModelFactory):
     class Meta:
         model = Product
+        exclude= ["_name_base"]
         sqlalchemy_session = get_scoped_session()
         sqlalchemy_session_persistence = "commit"
 
-    name = Faker("sentence", nb_words=2)
+    _name_base = Faker("words", nb=2)
+
+
+    name = LazyAttribute(lambda p: f"{' '.join(p._name_base)} pizza") 
     base_price = Faker("random_number", digits=4)
     base_size = Faker(
-        "random_elements", elements=["small", "medium", "large", "xl", "party"]
+        "random_element", elements=["small", "medium", "large", "xl", "party"]
     )
     is_draft = Faker("boolean", chance_of_getting_true=10)
     stock = Faker("random_number", digits=2)
--- seed.py
+++ seed.py
@@ -3,24 +3,28 @@ from db.factories.product_factory import ProductFactory
 from sqlalchemy import delete
 from sqlalchemy.orm import Session
 from db.setup import get_db
-from db.models import User
+from db.models import User, Product
 
 if __name__ == "__main__":
     # reset db
-    print("Resetting DB...")
+    print("Resetting DB.")
     session: Session = next(get_db())
     stmt = delete(User)
     session.execute(stmt)
     session.commit()
-    print("Resetting DB: DONE")
+
+    stmt = delete(Product)
+    session.execute(stmt)
+    session.commit()
+    print("Resetting DB DONE")
 
     # seed
-    print("Seeding DB...")
-    print("Seeding Users...")
+    print("Seeding DB.")
+    print("Seeding Users.")
     UserFactory.create_batch(50)
-    print("Seeding Users: Done")
-    print("Seeding DB: DONE")
+    print("Seeding Users Done")
 
-    print("Seeding Users...")
+    print("Seeding Products.")
     ProductFactory.create_batch(50)
-    print("Seeding Users: Done")
+    print("Seeding Done")
+    print("Seeding DB DONE")
