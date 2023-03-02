8d0626fad9fa2c1d437ab6f6910aa4fb6816d27e
Jonathan Osmond <jonathancosmond@gmail.com>
2023-03-02 14:34:31 -0500

    improve base_price productfacory values

--- db/factories/product_factory.py
+++ db/factories/product_factory.py
@@ -7,15 +7,15 @@ from db.models import User, Product
 class ProductFactory(SQLAlchemyModelFactory):
     class Meta:
         model = Product
-        exclude= ["_name_base"]
+        exclude = ["_name_base", "_price_base"]
         sqlalchemy_session = get_scoped_session()
         sqlalchemy_session_persistence = "commit"
 
     _name_base = Faker("words", nb=2)
+    _price_base = Faker("random_number", digits=4)
 
-
-    name = LazyAttribute(lambda p: f"{' '.join(p._name_base)} pizza") 
-    base_price = Faker("random_number", digits=4)
+    name = LazyAttribute(lambda p: f"{' '.join(p._name_base)} pizza")
+    base_price = LazyAttribute(lambda p: p._price_base / 100)
     base_size = Faker(
         "random_element", elements=["small", "medium", "large", "xl", "party"]
     )
