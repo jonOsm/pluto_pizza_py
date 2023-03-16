a14ae1cec8344cdf085c6b8a0be65434036369c8
Jonathan Osmond <jonathancosmond@gmail.com>
2023-03-16 14:51:16 -0400

    update user factory password_hash to adhere to pydantic validators

--- db/factories/user_factory.py
+++ db/factories/user_factory.py
@@ -1,26 +1,24 @@
 from factory import Faker, RelatedFactoryList
 from factory.alchemy import SQLAlchemyModelFactory
-from db.setup import scoped_session_local 
-from db.models import AddressModel, UserModel
+from db.setup import scoped_session_local
+from db.models import UserModel
 from .address_factory import AddressFactory
+
 
 class UserFactory(SQLAlchemyModelFactory):
     class Meta:
         model = UserModel
-        sqlalchemy_session = scoped_session_local 
+        sqlalchemy_session = scoped_session_local
         sqlalchemy_session_persistence = "commit"
 
     # username = factory.Sequence(lambda n: "john%s" % n)
     # email = factory.LazyAttribute(lambda o: "%s@example.org" % o.username)
     # date_joined = factory.LazyFunction(datetime.datetime.now)
 
-    email = Faker('ascii_email')
-    email_verified = Faker('boolean', 
-                           chance_of_getting_true=75)
-    first_name = Faker('first_name')
-    last_name = Faker('last_name')
-    disabled = Faker('boolean', 
-                     chance_of_getting_true=5)
-    addresses = RelatedFactoryList(AddressFactory,'user', 2)
-
-    hashed_password = "$2b$12$RjCfW4z96wrp2isyFMiTweQL/H/QaaKY8FjrH9/1F/HOiH6y4s4Qy"
+    email = Faker("ascii_email")
+    email_verified = Faker("boolean", chance_of_getting_true=75)
+    first_name = Faker("first_name")
+    last_name = Faker("last_name")
+    disabled = Faker("boolean", chance_of_getting_true=5)
+    addresses = RelatedFactoryList(AddressFactory, "user", 2)
+    hashed_password = "$2b$12$yQGJX8bHSq1Hr/ZSJ.7S8uTgZmAGKCdFqyI4Pm2w1WeKD.nJDCqs6"
